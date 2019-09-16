23faa8134f02af8fbb41e1a9d30c0df67aec6872
Jakub Kiełbasa <jakub.kiel@gmail.com>
2019-09-16 21:58:41 +0200

    Place code between {}

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+{
+    let computerMove = `kamień`;
+    let playerMove = `papier`;
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+}
