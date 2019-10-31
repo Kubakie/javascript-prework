41c7f768c70f3a9a3b93abfb17770856d5a27c18
Jakub Kiełbasa <jakub.kiel@gmail.com>
2019-10-31 22:00:01 +0100

    Add variable playerMove  in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 {
-    let computerMove = `kamień`;
-    let playerMove = `papier`;
+    let computerMove = '`kamień';
+    let playerMove = 'papier';
 
     printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
 }
